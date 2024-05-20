9fa1a22129b117f998013e5bdbb6f44699165690
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-20 16:50:15 +0530

    Added Counter component, deleted local file, cloaned git repo, installed react script.

---
+++ src/Components/Counter/Counter.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './counter.css'
+
+
+class Counter extends Component{
+    render(){
+        return(
+            <div>
+                Hellooo.
+                <br />
+                <button>click.</button>
+            </div>
+        );
+    }
+}
+export default Counter
