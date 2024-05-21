17faeb421f438d62ea8d98363dd666e64ff1f01a
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-21 15:54:07 +0530

    Added intial state in constructor and the state will update from mthd binded.

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -3,18 +3,31 @@ import './counter.css'
 
 
 class Counter extends Component{
+    //define initial state in constructor. and update state in theh mthd.
+    constructor(){
+        super()
+        this.state=({
+            counter : 0
+        })
+        this.increment = this.increment.bind(this)          //bind mthd with const. 
+    }
+
+
     render(){
         return(
             <div>
-                Hellooo.
-                <br />
-                <button onClick={this.increment}>+1</button>
-                <span className="count">0</span>
+                 Hellooo.   
+                <br />                                     
+                <button onClick={this.increment}>+1</button>     {/*//call local mthd with this.  */}
+                <span className="count">{this.state.counter}</span>
             </div>
         );
     }
     increment(){
         console.log('increment')
+        this.setState({                     //set the state in the mthd. 
+            counter : this.state.counter + 1
+        })
     }
 }
 export default Counter
